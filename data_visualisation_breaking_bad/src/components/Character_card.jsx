240497f96afccd04855606e6e4ac78d0c60397ac
HannahBurks <hannahburks@hotmail.com>
2022-11-29 15:33:59 +0000

    this code will display pics of characters and info

--- data_visualisation_breaking_bad/src/components/Character_card.jsx
+++ data_visualisation_breaking_bad/src/components/Character_card.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
-
+import "./Character_card.css"
 
 function Character_card({charData
 }) {
-    console.log(charData.img)
+    console.log(charData)
   return (
     <div>
-        <img src={charData.img} alt={charData.name}></img>
+        <div className="front_card">
+        <a href="#" title="breaking_bad_photos">
+        <img src={charData.img} alt={charData.name} className="profile_pic"></img>
+        </a>
 <li key={charData.name}>{charData.name}</li>
+</div>
+<div className="back_card">
+<li key={charData.nickname}>Nickname: {charData.nickname}</li>
+<li key='jobs'>Jobs:</li>
+{charData.occupation.map((job) => {
+   return <li key={job}>{job}</li>
+})}
+<li key={charData.status}>Alive/dead: {charData.status}</li>
+<li key={charData.portrayed}>Portrayed by:{charData.portrayed}</li>
+</div>
     </div>
   )
 }
